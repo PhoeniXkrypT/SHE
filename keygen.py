12079792c7a219fea4f0423a71bd1719348c6088
archanabs <archanabs15@gmail.com>
2014-05-04 13:07:24 +0530

    Update keygen.py
    Adding comments to function

--- keygen.py
+++ keygen.py
@@ -4,20 +4,22 @@ bigOconstant = 2
 lambda_value = 8
 
 def main():
-	sk = secret_key(lambda_value)
+	sk = secret_key(lambda_value) # function call to generate secret key
 	print "Secret Key\n", int(sk)
-	pk = public_key(sk)
+	pk = public_key(sk) # function call to generate public key list
 	print "Public Key Values\n", pk
 
+# Function to calculate eta value from the security parameter lambda
 def eta(lambda_value):
 	return (bigOconstant * lambda_value * lambda_value)
-
+# Function to calculate gamma value from the security parameter lambda
 def gamma(lambda_value):
 	return (bigOconstant * lambda_value**5)
-
+# Function to calculate tau value from the security parameter lambda
 def tau(lambda_value):
 	return (gamma(lambda_value) + lambda_value)
 
+# Function to calculate the secret key which is an odd integer
 def secret_key(lambda_value):
 	eta_value = eta(lambda_value)
 	number_count = eta_value / 32
@@ -39,7 +41,7 @@ def distribution(secret_key):
 	r = int(numpy.random.uniform(-2**lambda_value,2**lambda_value))
 	x = secret_key * q + r
 	return x
-
+# Function to calculate the public key
 def public_key(secret_key):
 	while True:
 		pubkey = [distribution(secret_key) for i in range(tau(lambda_value))]
